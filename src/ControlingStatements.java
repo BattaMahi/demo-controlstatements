ac890b786e3c075fdd251e7fccd40e9340405a1b
BattaMahi <maheshbatta14@gmail.com>
2022-09-16 15:40:03 +0530

    Source added

---
+++ src/ControlingStatements.java
@@ -0,0 +1,245 @@
+
+public class ControlingStatements {
+
+	public static void main(String[] args) {
+		
+//		If statement
+//		int apples,grapes;
+//		
+//		apples = 5;
+//		grapes = 5;
+//		
+//		if(apples==grapes) {
+//			System.out.println("yes");
+//		}
+		
+//		int apples,grapes;
+//		
+//		apples = 10;
+//		grapes = 5;
+//		
+//		if(apples!=grapes) {
+//			System.out.println("yes");
+//		}
+		
+//		if else Statement
+		
+//		int apples,grapes;
+//		
+//		apples = 10;
+//		grapes = 5;
+//		
+//		if(apples==grapes) {
+//			System.out.println("yes");
+//		}else {
+//			System.out.println("NO");
+//		}
+		
+		
+//		int apples,grapes;
+//		
+//		apples = 10;
+//		grapes = 5;
+//		
+//		if(apples<grapes) {
+//			System.out.println("yes");
+//		}else {
+//			System.out.println("NO");
+//		}
+		
+//		int apples,grapes;
+//		
+//		apples = 10;
+//		grapes = 5;
+//		
+//		if(apples>grapes) {
+//			System.out.println("yes");
+//		}else {
+//			System.out.println("NO");
+//		}
+		
+//		int apples,grapes;
+//		
+//		apples = 10;
+//		grapes = 5;
+//		
+//		if(apples>grapes) {
+//			System.out.println("yes");
+//		}else {
+//			System.out.println("NO");
+//		}
+		
+//		int maths, physics, chemistry;
+//		
+//		maths = 50;
+//		physics = 25;
+//		chemistry = 45;
+//		
+//		if(chemistry<35) {
+//			System.out.println("Passed");
+//		}else {
+//			System.out.println("Failed");
+//			
+//		}
+		
+		
+//		int maths, physics, chemistry;
+//		
+//		maths = 50;
+//		physics = 25;
+//		chemistry = 45;
+//		
+//		if(maths>35&& physics>35 &&chemistry<35) {
+//			System.out.println("Passed");
+//		}else {
+//			System.out.println("Failed");
+//			
+//		}
+		
+//		int maths, physics, chemistry;
+//		
+//		maths = 50;
+//		physics = 25;
+//		chemistry = 45;
+//		
+//		if(maths>35 && physics>18 &&chemistry>35) {
+//			System.out.println("Passed");
+//		}else {
+//			System.out.println("Failed");
+//			
+//		}
+		
+//		int telugu, hindi;
+//		
+//		telugu = 65;
+//		hindi  = 15;
+//		
+//		
+//		if(telugu > 30 || hindi >35) {
+//			System.out.println("Passed");
+//		}else {
+//			System.out.println("Failed");
+//		}
+		
+		
+//		int telugu, hindi;
+//		
+//		telugu = 20;
+//		hindi  = 15;
+//		
+//		
+//		if(telugu > 30 || hindi >30) {
+//			System.out.println("Passed");
+//		}else {
+//			System.out.println("This is else");
+//		}
+		
+//		int age;
+//		age = 18;
+//		
+//		if(age==18) {
+//			System.out.println("You can vote now");
+//		};
+//		if(age==25) {
+//			System.err.println("You can Marry now");
+//		};
+//		if(age==60) {
+//			System.out.println();
+//		}
+//		
+//		int age;
+//		age = 18;
+//		
+//		switch (age){
+//			
+//		case 18:
+//			System.out.println("You can vote now");
+//			
+//			
+//		case 25:
+//			System.out.println("you can marry now");
+//			
+//			
+//		case 60:
+//			System.out.println("you can retire now");
+//			
+//			
+//		default:
+//			System.out.println("You have to wait");
+//		}
+		
+//		nested if
+//		int age = 10;
+//		
+//		if(age>=18) {
+//			System.out.println("You can vote");
+//		}else {
+//			System.out.println("You have to wait");
+//			if(age>12) {
+//				System.out.println("you are too young");
+//			}else {
+//				System.out.println("you are a kid");
+//			}
+//			
+//		}
+		
+		
+		
+//		int telugu = 60;
+//		int hindi  = 30;
+//		
+//		if(telugu>35) {
+//			if(hindi>18) {
+//				System.out.println("Passed");
+//			}else {
+//				System.out.println("Failed");
+//			}
+//		
+//		}else {
+//			System.out.println("Failed");
+//		}
+		
+		
+//		int telugu = 20;
+//		int hindi  = 20;
+//		
+//		if(telugu>35) {
+//			if(hindi>18) {
+//				System.out.println("Passed");
+//			}else {
+//				System.out.println("Failed in hindi");
+//			}
+//		
+//		}else {
+//			System.out.println("Failed in telugu or both");
+//		}
+		
+//		Else if
+//		int age ;
+//		age = 19;
+//		
+//		if(age<18) 
+//			System.out.println("you are too young");
+//			
+//		else if (age==18) 
+//			System.out.println("you can vote");
+//		else if (age==50) 
+//			System.out.println("You are old");
+//		else if (age>60) 
+//			System.out.println("you are too old");
+//		else
+//			System.out.println("who are you");
+		
+		
+		int age ;
+		age = 55;
+		if(age<18) {
+			
+		}
+		
+		
+		
+
+	}
+
+}
